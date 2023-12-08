fd01fd76a0757209926563a03b59595298aa3509
sheilahanson <slhanson11@live.com>
2023-12-07 22:23:35 -0600

    added fetch recipe api and html recipe button

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,3 +17,20 @@ function fetchNutrientApi() {
       console.log(data);
     });
 }
+
+ var recipesBtn = document.querySelector('.recipes-btn');
+
+ recipesBtn.addEventListener('click', function(){
+  fetchRecipeApi().then(function(result) {
+    console.log(result);
+  });
+ });
+ 
+ async function fetchRecipeApi() {
+  var inputSection = document.querySelector(".input-section");
+  var value = inputSection.value;
+  var response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${encodeURIComponent(value)}&app_id=df46ca95&app_key=277fe705327a2981fb85ba1e1202742a`);
+  var result = await response.json();
+  return result;
+}
+
