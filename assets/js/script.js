c5afa71dbb82d3bb625efa9fe25a6a6513ae87dd
salidamaharjan <salidamaharjan@outlook.com>
2023-12-07 21:24:45 -0600

    created function to fetch nutritional API url

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,19 @@
 var searchBtn = document.querySelector(".search-btn");
 
-searchBtn.addEventListener("click", function(){
-    
-})
+searchBtn.addEventListener("click", function () {
+  fetchNutrientApi();
+});
+function fetchNutrientApi() {
+  var inputSection = document.querySelector(".input-section");
+  var value = inputSection.value;
+  var nutrientURL = `https://api.edamam.com/api/food-database/v2/parser?app_id=f02972e7&app_key=3d2353afd7e7eccce279b9f2bb359688&ingr=${encodeURIComponent(
+    value
+  )}&nutrition-type=cooking`;
+  fetch(nutrientURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+}
