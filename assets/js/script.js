0087ff1d19b333af844743c62d1749482af11ea5
salidamaharjan <salidamaharjan@outlook.com>
2023-12-10 21:44:09 -0600

    displayed modal in UI

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,15 +45,14 @@ function fetchNutrientApi() {
         $("#foodName").text(firstHint.food.label);
         $("#foodCategory").text(firstHint.food.category);
         // Clear any previous image from the container
-        $('#foodImage').html("");
+        $("#foodImage").html("");
         // Create an image tag
-        var foodImage = $('<img />',
-        { 
+        var foodImage = $("<img />", {
           // Set the image src to the image URL from the API
-          src: firstHint.food.image
+          src: firstHint.food.image,
         });
-        // Append the image to <p id="foodImage"></p> 
-        foodImage.appendTo($('#foodImage'));
+        // Append the image to <p id="foodImage"></p>
+        foodImage.appendTo($("#foodImage"));
         // Log nutritional information
         if (firstHint.food.nutrients) {
           var nutrients = firstHint.food.nutrients;
@@ -78,16 +77,16 @@ function saveIngredientToLocalStorage(ingredient) {
     JSON.parse(localStorage.getItem("savedIngredients")) || [];
   console.log("Saved ingredients before:", savedIngredients);
   // making sure the saved ingredient do not duplicate in list
-  if(!savedIngredients.includes(ingredient) && ingredient.length >0) {
+  if (!savedIngredients.includes(ingredient) && ingredient.length > 0) {
     // Add the new ingredient to the top of the Array
     savedIngredients.unshift(ingredient);
     //Maximum number of history items is 15
     if (savedIngredients.length > 15) {
-        savedIngredients = savedIngredients.slice(0, 15);
+      savedIngredients = savedIngredients.slice(0, 15);
     }
     // Save the updated array back to local storage
     localStorage.setItem("savedIngredients", JSON.stringify(savedIngredients));
-    
+
     console.log("Ingredient saved to local storage:", ingredient);
 
     // Update the displayed saved ingredients
@@ -111,21 +110,29 @@ function updateSavedIngredients() {
   // Display each saved ingredient in the list
   savedIngredients.forEach(function (ingredient) {
     var button = document.createElement("button");
-    button.classList.add("button", "is-medium", "is-primary", "mr-2", "is-fullwidth", "has-text-weight-bold", "is-size-4");
+    button.classList.add(
+      "button",
+      "is-medium",
+      "is-primary",
+      "mr-2",
+      "is-fullwidth",
+      "has-text-weight-bold",
+      "is-size-4"
+    );
     button.textContent = ingredient;
 
     // Attach a click event listener to each button
     button.addEventListener("click", function () {
       // Handle the click event
       console.log("Button clicked for ingredient:", ingredient);
-      
+
       console.log(searchBtn);
     });
     savedIngredientsContainer.appendChild(button);
     savedIngredientsContainer.classList.add("is-flex-direction-column");
   });
 
-  var inputSection = document.querySelector('#input');
+  var inputSection = document.querySelector("#input");
   var recipesBtn = document.querySelector(".recipes-btn");
 
   recipesBtn.addEventListener("click", async function () {
@@ -163,28 +170,48 @@ function constructRecipeBoxInfo(recipe) {
                     >
                       ${recipe.label}
                     </div>
-                    <img class="recipe-image" src="${recipe.images.THUMBNAIL.url}" alt="recipe image" />
+                    <img class="recipe-image" src="${
+                      recipe.images.THUMBNAIL.url
+                    }" alt="recipe image" />
                     <ul class="recipe-nutrients">
                       <li class="recipe-calorie has-text-white is-size-7">
-                        Calorie: <span>${Math.round(recipe.totalNutrients.ENERC_KCAL.quantity)} ${recipe.totalNutrients.ENERC_KCAL.unit}</span> 
+                        Calorie: <span>${Math.round(
+                          recipe.totalNutrients.ENERC_KCAL.quantity
+                        )} ${recipe.totalNutrients.ENERC_KCAL.unit}</span> 
                       </li>
                       <li class="recipe-protein has-text-white is-size-7">
-                        Protein: <span>${Math.round(recipe.totalNutrients.PROCNT.quantity)} ${recipe.totalNutrients.PROCNT.unit}</span>
+                        Protein: <span>${Math.round(
+                          recipe.totalNutrients.PROCNT.quantity
+                        )} ${recipe.totalNutrients.PROCNT.unit}</span>
                       </li>
                       <li class="recipe-fat has-text-white is-size-7">
-                        Fat: <span>${Math.round(recipe.totalNutrients.FAT.quantity)} ${recipe.totalNutrients.FAT.unit}</span>
+                        Fat: <span>${Math.round(
+                          recipe.totalNutrients.FAT.quantity
+                        )} ${recipe.totalNutrients.FAT.unit}</span>
                       </li>
                       <li class="recipe-carbs has-text-white is-size-7">
-                        Carbohydrates: <span>${Math.round(recipe.totalNutrients.CHOCDF.quantity)} ${recipe.totalNutrients.CHOCDF.unit}</span>
+                        Carbohydrates: <span>${Math.round(
+                          recipe.totalNutrients.CHOCDF.quantity
+                        )} ${recipe.totalNutrients.CHOCDF.unit}</span>
                       </li>
                       <li class="recipe-fiber has-text-white is-size-7">
-                        Fiber: <span>${Math.round(recipe.totalNutrients.FIBTG.quantity)} ${recipe.totalNutrients.FIBTG.unit}</span>
+                        Fiber: <span>${Math.round(
+                          recipe.totalNutrients.FIBTG.quantity
+                        )} ${recipe.totalNutrients.FIBTG.unit}</span>
                       </li>
                     </ul>
                     <button class="button btn-recipe-view is-small mt-3">
                       View Recipe
                     </button>
                   </div>`;
+//recipe button is only found in the div element because it is not attached to HTML yet
+  var recipeBtn = divEl.querySelector(".btn-recipe-view");
+  recipeBtn.addEventListener("click", function() {
+    var recipeModal = document.querySelector(".recipe-view-modal");
+    //adding class is-active to show the modal in UI
+    recipeModal.classList.add("is-active");
+    
+  })
   return divEl;
 }
 
@@ -193,12 +220,12 @@ async function fetchRecipeApi() {
   var value = inputElement.value;
   //strip out anything other than letters and spaces
   value = value.replace(regexNotLettersSpaces, "");
-  var response = await fetch(
-    `https://api.edamam.com/api/recipes/v2?type=public&q=${encodeURIComponent(
-      value
-    )}&app_id=df46ca95&app_key=277fe705327a2981fb85ba1e1202742a`
-  );
-  // var response = await fetch(`./assets/js/recipe.json`);
+  // var response = await fetch(
+  //   `https://api.edamam.com/api/recipes/v2?type=public&q=${encodeURIComponent(
+  //     value
+  //   )}&app_id=df46ca95&app_key=277fe705327a2981fb85ba1e1202742a`
+  // );
+  var response = await fetch(`./assets/js/recipe.json`);
   var result = await response.json();
   return result;
 }
